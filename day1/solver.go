971b4dbbec8a75ef309bc3ad0c6257ba3d99f36f
cakuang1 <cakuang1@berkeley.edu>
2023-12-08 14:36:39 -0800

    day 1 completed

--- day1/solver.go
+++ day1/solver.go
@@ -11,20 +11,25 @@ import (
 
 // CODE FOR DAY 1
 
-func digitinstring(line string)(int){
-	foundfirst := false
-	var first,last rune
-	for _,e  := range line {
+
+func digitinstring(line string, this map[string]string)(int){
+	var sliceofletters []string
+	for i,e  := range line {
 		if unicode.IsDigit(e) {
-			if !foundfirst {
-				foundfirst = true
-				first = e
+			sliceofletters = append(sliceofletters, string(e))
+		 }else {
+			for key,value := range this {
+				length := len(key)
+				if i + length <= len(line) {
+					slice := line[i : i + length]
+					if slice == key {
+						sliceofletters = append(sliceofletters, value)
+					}
+				}
 			}
-			last = e
 		}
-	}
-	
-	concat,err := strconv.Atoi(string(first) + string(last))
+		}
+	concat,err := strconv.Atoi(sliceofletters[0] + sliceofletters[len(sliceofletters) - 1])
 	if err != nil {
 		fmt.Println("error",err)
 		os.Exit(1)
@@ -34,6 +39,16 @@ func digitinstring(line string)(int){
 
 
 func main() {
+	myMap := make(map[string]string)
+	myMap["one"] = "1"
+	myMap["two"] = "2"
+	myMap["three"] = "3"
+	myMap["four"] = "4"
+	myMap["five"] = "5"
+	myMap["six"] = "6"
+	myMap["seven"] = "7"
+	myMap["eight"] = "8"
+	myMap["nine"] = "9"
 	file, err := os.Open("puzzle.txt")
 	if err != nil {
 		fmt.Println("Error occured", err)
@@ -41,13 +56,11 @@ func main() {
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	
 	sum := 0
-
 	for scanner.Scan() {
 		line := scanner.Text()
-		sum += digitinstring(line)
-
+		sum += digitinstring(line,myMap)
+		
 	}
 	fmt.Println(sum)
 }
